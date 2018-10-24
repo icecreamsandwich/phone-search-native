fc1fd893f218bb64df12dc0d3ed40e392efa0ac5
CrazyRubix <muneebkt@gmail.com>
2018-10-24 12:26:44 +0530

    mods to the form elements

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -16,15 +16,13 @@ import {
   FlatList,
   Picker
 } from 'react-native';
-import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
-import RangeSlider from 'react-native-range-slider';
-
+import { FormLabel, FormInput, FormValidationMessage, Slider } from 'react-native-elements';
 export default class HomeScreen extends Component {
   constructor(props){
     super(props);
     this.state = {
       brandName: ""
-      priceRange: ""
+      priceRange: []
       brand: "",
       showBrand:false,
       isLoading:true
@@ -42,7 +40,7 @@ export default class HomeScreen extends Component {
   componentdidMount(){
     fetch('http://localhost/gsmarena-API/api/?action=brands')
       .then(response => response.json())
-      .then(data => this.setState({ brands:data.title }));
+      .then(data => this.setState({ brands:data }));
       this.setState({isLoading:false})
   }
 
@@ -53,9 +51,11 @@ export default class HomeScreen extends Component {
   handleButtonPress() {
          //log all form data
          alert(this.state.brand+"=="+this.state.priceRange);
+         var nPriceMin = priceRange[0];
+         var nPriceMax = priceRange[1];
          return false;
          //fetch request to get filtered data
-         fetch('http://localhost/gsmarena-API/api/?action=brands&brandName='+brand+'&nPriceMin='+100+'&nPriceMax='+4000)
+         fetch('http://localhost/gsmarena-API/api/?action=brands&sMakers='+brand+'&nPriceMin='+nPriceMin+'&nPriceMax='+nPriceMax)
            .then(response => response.json())
            .then(data => this.setState({ filteredBrands:data }));
         this.setState({showBrand:true})
@@ -94,48 +94,46 @@ export default class HomeScreen extends Component {
     return (
       <View style={styles.container}>
 
-        <Picker selectedValue = {} onValueChange = {(brand) => this.setState({brand})}>
+        <Picker selectedValue = {} onValueChange = {(brand) => this.setState({brandId})}>
           { this.state.brands.map(function(brand,i){
+            var brandHref = brand.href;
+            var brandId = parseInt(brandHref, 10);
+
             return (
-              <Picker.Item label = "{brand}" value = "{brand}" key={i} />
+              <Picker.Item label="{brand}" value="{brandId}" key={i} />
             );
           }, this)}
 
         </Picker>
 
-        <RangeSlider
-          minValue={0}
-          maxValue={50000}
-          tintColor={'#da0f22'}
-          handleBorderWidth={1}
-          handleBorderColor="#454d55"
-          selectedMinimum={0}
-          selectedMaximum={25000}
-          style={{ flex: 1, height: 70, padding: 10, backgroundColor: '#ddd' }}
-          onChange={ (data)=>{ this.setState({priceRange})} }
-        />
+        <Slider
+          value={this.state.value}
+          minimumValue={0}
+          maximumValues={400}
+          onValueChange={(priceRange) => this.setState({priceRange})} />
 
         <Button
           raised
           icon={{name: 'check'}}
           title='SUBMIT'
           onPress={() => this.handleButtonPress()} />
+
+        {if(this.state.showBrand){
+          <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+            <View style={styles.getStartedContainer}>
+              <Text style={styles.getStartedText}> Brands </Text>
+            </View>
+
+            <FlatList
+              data={this.state.filteredBrands}
+              keyExtractor={(item, i) => String(i)}
+              renderItem={this._renderItem}
+              ItemSeparatorComponent={this._ItemSeparator}
+            />
+          </ScrollView>
+        }}
+
       </View>
-       {if(this.state.showBrand){
-         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-           <View style={styles.getStartedContainer}>
-             <Text style={styles.getStartedText}> Brands </Text>
-           </View>
-
-           <FlatList
-             data={this.state.filteredBrands}
-             keyExtractor={(item, i) => String(i)}
-             renderItem={this._renderItem}
-             ItemSeparatorComponent={this._ItemSeparator}
-           />
-         </ScrollView>
-       }}
-
     );
   }
 }
@@ -144,4 +142,76 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  button: {
+    marginBottom: 15,
+    padding: 15,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#666666',
+    fontSize: 18
+  },
+  item: {
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 8,
+  },
+  avatar: {
+    height: 36,
+    width: 36,
+    borderRadius: 18,
+    backgroundColor: '#e91e63',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  letter: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  details: {
+    margin: 8,
+  },
+  name: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: 'black',
+  },
+  number: {
+    fontSize: 12,
+    color: '#999',
+  },
+  separator: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: 'rgba(0, 0, 0, .08)',
+  }
 });
